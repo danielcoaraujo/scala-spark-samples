455e4a2a77e92ce5f39b306e2e4a6116775c27a5
Araujo <danielcoaraujo@gmail.com>
2018-10-18 17:07:09 -0300

    - Small refactors - Added another approach to the same purpose.

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -13,11 +13,14 @@ object WordCount {
 			.flatMap(word => word.toLowerCase.split("\\W+"))
 			.countByValue()
 			.toSeq
+		words.sortBy(map => map._2).foreach(println)
 
-		words
-			.map(map => (map._2, map._1))
-			.sorted
-			.foreach(println)
+
+//		//Another approach, check if this approach is better or not
+//		val words = lines.flatMap(word => word.toLowerCase.split("\\W+"))
+//		val wordsCount = words.map(word => (word,1)).reduceByKey((x,y) => x + y)
+//		val sortedWords = wordsCount.map(x => (x._2,x._1)).sortByKey()
+//		sortedWords.map(map => (map._2, map._1)).foreach(println)
 	}
 
 }
