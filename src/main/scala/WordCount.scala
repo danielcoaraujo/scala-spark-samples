89e1a9285a09067113e614b0902f96022d68b2e1
danielcoaraujo <danielcoaraujo@gmail.com>
2018-10-18 21:07:11 -0300

    Fixing the another approach, it must have the "collect" after sortByKey.

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -19,7 +19,7 @@ object WordCount {
 //		//Another approach, check if this approach is better or not
 //		val words = lines.flatMap(word => word.toLowerCase.split("\\W+"))
 //		val wordsCount = words.map(word => (word,1)).reduceByKey((x,y) => x + y)
-//		val sortedWords = wordsCount.map(x => (x._2,x._1)).sortByKey()
+//		val sortedWords = wordsCount.map(x => (x._2,x._1)).sortByKey().collect
 //		sortedWords.map(map => (map._2, map._1)).foreach(println)
 	}
 
