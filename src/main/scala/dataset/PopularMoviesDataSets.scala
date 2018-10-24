ead066ab8024771ebf425a15e2b1b14b6c81631b
danielcoaraujo <danielcoaraujo@gmail.com>
2018-10-23 23:04:33 -0300

    adding a map of names of the movies

--- src/main/scala/dataset/PopularMoviesDataSets.scala
+++ src/main/scala/dataset/PopularMoviesDataSets.scala
@@ -8,8 +8,8 @@ object PopularMoviesDataSets {
 
 	def parseToMovie(line: String) : Movie = {
 		val fields = line.split("\t")
-		val id = fields(0).toInt
-		val personId = fields(1).toInt
+        val id = fields(1).toInt
+        val personId = fields(0).toInt
 		val rating = fields(2).toInt
 		val timestamp = fields(3).toInt
 		Movie(id, personId, rating, timestamp)
@@ -24,13 +24,37 @@ object PopularMoviesDataSets {
 			.getOrCreate()
 
 		import spark.implicits._
-
 		val movies = spark.sparkContext
 			.textFile("./resources/u.data")
 			.map(parseToMovie)
 			.toDS()
 			.cache()
 
-		movies.printSchema()
-	}
+        val topMovies = movies
+            .groupBy("id")
+            .count()
+            .orderBy($"count".desc)
+            .take(10)
+            .foreach(println)
+
+        val mapIdName = loadMovieNames(spark)
+
+//        for(movie <- topMovies){
+//            println(movie(0), mapIdName.asInstanceOf[Int])
+//        }
+
+        spark.stop()
+    }
+
+    def loadMovieNames(sparkSession: SparkSession) = {
+        val lines = sparkSession
+            .sparkContext
+            .textFile("./resources/u.item")
+            .map(line => {
+                val fields = line.split("\\|")
+                val id = fields(0).toInt
+                val name = fields(1)
+                (id, name)
+            }).collect().toMap
+    }
 }
